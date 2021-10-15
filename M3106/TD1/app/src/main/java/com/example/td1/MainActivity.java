80a58c9d7360dec5d70d5791069f70658134235f
Vincent Diribarne <vdiribarnepro@gmail.com>
2021-10-15 14:24:04 +0200

    Fin TD1

--- M3106/TD1/app/src/main/java/com/example/td1/MainActivity.java
+++ M3106/TD1/app/src/main/java/com/example/td1/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.td1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -16,30 +16,63 @@ public class MainActivity extends AppCompatActivity {
     private Button bouton;
     private EditText editText;
     private LinearLayout container;
+    private EditText pseudo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bouton = (Button) findViewById(R.id.button);
-        editText = (EditText) findViewById(R.id.editTextMessage);
-        container = (LinearLayout) findViewById(R.id.container);
+        bouton = findViewById(R.id.button);
+        editText = findViewById(R.id.editTextMessage);
+        container = findViewById(R.id.container);
+        pseudo = findViewById(R.id.pseudo);
 
         bouton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onBackPressed();
+                if (pseudo.getText().toString().isEmpty()) {
+                    pseudo.setError("Ajoutez un pseudo !");
+                    return;
+                }
+
+                if (editText.getText().toString().isEmpty()) {
+                    editText.setError("Ajoutez un message !");
+                    return;
+                }
+                createTextChat();
                 Toast.makeText(getApplicationContext(), "Votre texte a été ajouté", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    if (pseudo.getText().toString().isEmpty()) {
+                        pseudo.setError("Ajoutez un pseudo !");
+                        return false;
+                    }
+
+                    if (editText.getText().toString().isEmpty()) {
+                        editText.setError("Ajoutez un message !");
+                        return false;
+                    }
+                    createTextChat();
+                    Toast.makeText(getApplicationContext(), "Votre texte a été ajouté", Toast.LENGTH_LONG).show();
+                }
+                return false;
             }
         });
     }
 
-    public void onBackPressed() {
-        TextView editTextAjout = new TextView(this);
-        container.addView(editTextAjout);
-        editTextAjout.setTextSize(25);
-        editTextAjout.setText(editText.getText());
+    public void createTextChat() {
+        TextView editTextAdd = new TextView(this);
+        container.addView(editTextAdd);
+        editTextAdd.setTextSize(20);
+        editTextAdd.setText(pseudo.getText() + " : " + editText.getText());
+        pseudo.setText("");
+        editText.setText("");
     }
 
 }
