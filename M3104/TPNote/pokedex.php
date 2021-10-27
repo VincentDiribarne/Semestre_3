1ae9cce3a4120b207517f0694fa2352bccff02fc
Vincent DIRIBARNE <vdiribarnepro@gmail.com>
2021-10-27 11:52:01 +0200

    TODO 9

--- M3104/TPNote/pokedex.php
+++ M3104/TPNote/pokedex.php
@@ -30,18 +30,17 @@
                     array_push($arguments, $_POST['type1']);
                 }
 
-                if ($_POST['condition'] != "EMPTY" && $_POST['operator'] != "EMPTY" && $_POST['number'] != "EMPTY") {
-                    $variable = $_POST['condition'] . $_POST['operator'] . $_POST['number'];
+                if ($_POST['condition'] != "EMPTY" && $_POST['operator'] != "EMPTY" && $_POST['MinMaxValue'] != "EMPTY") {
+                    $variable = $_POST['condition'] . $_POST['operator'] . $_POST['MinMaxValue'];
                     $sql .= " AND {$variable}";
                     array_push($arguments, $_POST['condition']);
                 }
 
                 if ($_POST['trier'] != "EMPTY") {
-                    $variable = $_POST['trier'] . " ". $_POST['groupe'];
+                    $variable = $_POST['trier'] . " " . $_POST['groupe'];
                     $sql .= " ORDER BY {$variable}";
                     array_push($arguments, $_POST['trier']);
                 }
-
 
                 $reponse = $bdd->prepare($sql);
 
@@ -53,6 +52,35 @@
                         echo $tmp->printHTML();
                     }
                 }
+
+            } catch (PDOExeption $err) {
+                echo "Erreur : " . $err->getMessage() . "<br />";
+                die();
+            }
+        }
+
+        if (isset($_POST['modifier']) && $_POST['MinMaxId'] != "EMPTY" && !empty($_POST['statistiqueModif']) && !empty($_POST['NouvelleValeur'])) {
+            try {
+                $bdd = new PDO("mysql:host=localhost;dbname=$db_name", $user, $pass);
+                $variable = "{$_POST['statistiqueModif']} = {$_POST['NouvelleValeur']} WHERE id = {$_POST['MinMaxId']}";
+                $sql = "UPDATE `pokedex` SET {$variable}";
+
+                $rep = $bdd->prepare($sql);
+                $rep->execute();
+
+
+                $sql2 = "SELECT * FROM `pokedex` WHERE id = {$_POST['MinMaxId']}";
+                $reponse2 = $bdd->prepare($sql2);
+
+                if (is_object($reponse2)) {
+                    $reponse2->execute();
+                    $tmp = $reponse2->fetchAll();
+                    foreach ($tmp as $ligne) {
+                        $tmp = new Pokemon($ligne['id'], $ligne['nom'], $ligne['types0nom'], $ligne['types0couleur'], $ligne['types1nom'], $ligne['types1couleur'], $ligne['baseHP'], $ligne['baseAttack'], $ligne['baseDefense'], $ligne['baseSpAttack'], $ligne['baseSpDefense'], $ligne['baseSpeed'], $ligne['description'], $ligne['image']);
+                        echo $tmp->printHTML();
+                    }
+                }
+
             } catch (PDOExeption $err) {
                 echo "Erreur : " . $err->getMessage() . "<br />";
                 die();
@@ -126,7 +154,7 @@
                     <option value=" < "><</option>
                     <option value=" <= "><=</option>
                 </select>
-                <input name="number" type="number" name="MinMaxValue" min="0" max="300"><br>
+                <input type="number" name="MinMaxValue" min="0" max="300"><br>
                 Trier par : <select name="trier">
                     <option value="EMPTY">---</option>
                     <option value="id">ID</option>
@@ -143,6 +171,25 @@
                 <label for="decroissant">Décroissant</label></br>
 
                 <input class="btn" type="submit" name="tri" value="Afficher les résultats">
+            </form>
+
+            <form method="post" class=cadre>
+                Modifier une stat : <br>
+                Id du Pokemon :
+                <input name="MinMaxId" type="number" min="0" max="300"> <br>
+                Stats : <select name="statistiqueModif">
+                    <option value="EMPTY">---</option>
+                    <option value="id">ID</option>
+                    <option value="baseHP">HP</option>
+                    <option value="baseAttack">ATQ</option>
+                    <option value="baseDefense">DEF</option>
+                    <option value="baseSpAttack">SP_ATQ</option>
+                    <option value="baseSpDefense">SP_DEF</option>
+                    <option value="baseSpeed">SPEED</option>
+                </select> <br>
+                Nouvelle Valeur :
+                <input name="NouvelleValeur" type="number" min="0" max="300"><br>
+                <input class="btn" type="submit" name="modifier" value="Modifier">
             </form>
         </div>
     </div>
