1ca0495e91ad1970cc46092340ef8c9e2f511503
Vincent Diribarne <vdiribarnepro@gmail.com>
2021-11-16 09:55:27 +0100

    Ajout de la v2

---
+++ M3105/TD Refactoring initial-code/src/v2/Customer.java
@@ -0,0 +1,58 @@
+package v2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+
+	private String name;
+	private List<Rental> rentals = new ArrayList<Rental>();
+
+	public Customer (String name){ 
+		this.name = name;
+	}
+
+	public void addRental(Rental aRental) { 
+		rentals.add(aRental);
+	}
+
+	public String getName (){
+		return name; 
+	}
+
+	public String statement() {
+
+		String result = "Rental Record for " + getName() + "\n";
+	
+		for (Rental each : rentals) {
+			result += "\t"+ each.getMovie().getTitle()+ "\t" + each.getCharge() + "\n";
+		}
+		//add footer lines 
+		result += "Amount owed is " + getTotalCharge() + "\n";
+		result += "You earned " + getTotalFrequentRenterPoints() + " frequent renter points";
+		
+		return result;
+	}
+
+	private int getTotalFrequentRenterPoints() {
+		int result =0;
+		
+		for (Rental each : rentals) {
+			result += each.getFrequentRenterPoints();
+		}
+		return result;
+	}
+
+	private double getTotalCharge() {
+		double result = 0;
+		
+		for (Rental each : rentals) {
+			result += each.getCharge();
+		}
+		return result;
+	}
+	
+	
+
+}
+
---
+++ M3105/TD Refactoring initial-code/src/v2/Rental.java
@@ -0,0 +1,32 @@
+package v2;
+
+public class Rental {
+	
+	private Movie movie; 
+	private int daysRented;
+	
+	public Rental(Movie movie, int daysRented) { 
+		this.movie = movie;
+		this.daysRented = daysRented;
+	}
+	
+	public int getDaysRented() {
+		return daysRented; 
+	}
+	
+	public Movie getMovie() { 
+		return movie;
+	}
+
+	public double getCharge() {
+		return movie.getCharge(daysRented);
+	}
+
+	public int getFrequentRenterPoints() {
+		
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) { 
+			return 2;
+		}
+		return 1;
+	} 
+}
