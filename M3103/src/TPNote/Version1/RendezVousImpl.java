4361a569dc7db22e56bcff74b8c5f4b60e5f3d4b
Vincent Diribarne <vdiribarnepro@gmail.com>
2021-12-01 12:23:48 +0100

    Methode add, remove, update, et debut getBefore

--- M3103/src/TPNote/Version1/RendezVousImpl.java
+++ M3103/src/TPNote/Version1/RendezVousImpl.java
@@ -4,44 +4,83 @@ import myrendezvous.Rendezvous;
 
 import java.util.Calendar;
 
-public class RendezVousImpl  implements Rendezvous {
+public class RendezVousImpl implements Rendezvous {
+    private String description;
+    private String title;
+    private int duration;
+    private Calendar time;
+    private int tag;
+
+    public RendezVousImpl(String description, String title, int duration, Calendar time) {
+        this.description = description;
+        this.title = title;
+        this.duration = duration;
+        this.time = (Calendar) time.clone();
+        this.tag = (int) this.time.clone();
+    }
+
+    public RendezVousImpl(String title, int duration, Calendar time) {
+        this(null, title, duration, time);
+    }
+
     @Override
     public Calendar getTime() {
-        return null;
+        return time;
     }
 
     @Override
     public int getDuration() {
-        return 0;
+        return duration;
     }
 
     @Override
     public String getTitle() {
-        return null;
+        return title;
     }
 
     @Override
     public String getDescription() {
-        return null;
+        return description;
+    }
+
+    public int getTag() {
+        return tag;
     }
 
     @Override
     public void setTime(Calendar calendar) throws IllegalArgumentException {
-
+        this.time = calendar;
+        this.tag = (int) this.time.clone();
     }
 
     @Override
     public void setDuration(int i) throws IllegalArgumentException {
-
+        this.duration = i;
     }
 
     @Override
     public void setTitle(String s) throws IllegalArgumentException {
-
+        this.title = s;
     }
 
     @Override
     public void setDescription(String s) {
+        this.description = s;
+    }
 
+    public void setTag(int tag) {
+        this.tag = tag;
+    }
+
+    @Override
+    public RendezVousImpl clone() {
+        RendezVousImpl copie;
+        try {
+            copie = (RendezVousImpl) super.clone();
+            copie.time = (Calendar) this.time.clone();
+            return copie;
+        } catch (CloneNotSupportedException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- M3103/src/TPNote/Version1/RendezVousManagerImpl.java
+++ M3103/src/TPNote/Version1/RendezVousManagerImpl.java
@@ -1,37 +1,86 @@
 package TPNote.Version1;
 
+import TPNote.Version1.Exception.RendezVousExistant;
 import myrendezvous.Rendezvous;
 import myrendezvous.RendezvousManager;
 import myrendezvous.exceptions.RendezvousNotFound;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.TreeMap;
 
 public class RendezVousManagerImpl implements RendezvousManager {
+    TreeMap<Integer, RendezVousImpl> RendezVousManager = new TreeMap<>();
 
     @Override
-    public Rendezvous addRendezvous(Rendezvous rendezvous) {
-        return null;
+    public Rendezvous addRendezvous(Rendezvous rdv) {
+        RendezVousImpl rendezVous = new RendezVousImpl(rdv.getTitle(), rdv.getDescription(), rdv.getDuration(), rdv.getTime());
+        if (RendezVousManager.containsKey(rendezVous.getTag())) {
+            try {
+                throw new RendezVousExistant();
+            } catch (RendezVousExistant e) {
+                e.printStackTrace();
+            }
+        }
+        RendezVousManager.put(rendezVous.getTag(), rendezVous);
+        return rendezVous.clone();
     }
 
     @Override
     public void removeRendezvous(Rendezvous rendezvous) throws IllegalArgumentException, RendezvousNotFound {
+        if (!(rendezvous instanceof RendezVousImpl)) {
+            throw new IllegalArgumentException("L'argument fournit n'est pas le bon");
+        }
 
+        int tag = (((RendezVousImpl) rendezvous).getTag());
+        if (!(RendezVousManager.containsKey(tag))) {
+            throw new RendezvousNotFound("Le rendez-vous fournit n'existe pas");
+        }
+
+        RendezVousManager.remove(tag);
     }
 
     @Override
     public boolean removeRendezvous(Calendar calendar) {
-        return false;
+        if (!RendezVousManager.containsKey(calendar)) {
+            return false;
+        }
+
+        RendezVousManager.remove(calendar);
+        return true;
+    }
+
+    @Override
+    public Rendezvous updateRendezvous(Rendezvous rendezvous) throws RendezvousNotFound {
+        if (!(rendezvous instanceof RendezVousImpl)) {
+            throw new IllegalArgumentException("L'argument fournit n'est pas le bon");
+        }
+
+        RendezVousImpl newRendezVous = ((RendezVousImpl) rendezvous);
+
+        if (!(RendezVousManager.containsKey(newRendezVous.getTag()))) {
+            throw new RendezvousNotFound("Le rendez-vous fournit n'existe pas");
+        }
+
+        RendezVousImpl rendezVousModif = RendezVousManager.get(newRendezVous.getTag());
+
+        newRendezVous.setTitle(rendezvous.getTitle());
+        newRendezVous.setDescription(rendezvous.getDescription());
+        newRendezVous.setDuration(rendezvous.getDuration());
+
+        if (rendezVousModif.getTime() != newRendezVous.getTime()) {
+            RendezVousManager.remove(newRendezVous.getTag());
+            newRendezVous.setTime(rendezvous.getTime());
+            RendezVousManager.put(newRendezVous.getTag(), newRendezVous);
+        }
+
+        return newRendezVous.clone();
     }
 
     @Override
     public void removeAllRendezvousBefore(Calendar calendar) throws IllegalArgumentException {
 
-    }
-
-    @Override
-    public Rendezvous updateRendezvous(Rendezvous rendezvous) throws RendezvousNotFound {
-        return null;
     }
 
     @Override
@@ -41,7 +90,14 @@ public class RendezVousManagerImpl implements RendezvousManager {
 
     @Override
     public List<Rendezvous> getRendezvousBefore(Calendar calendar) throws IllegalArgumentException {
-        return null;
+        List<RendezVousImpl> rendezvous = new ArrayList<>();
+        List<Rendezvous> rendezVousClone = new ArrayList<>();
+
+        for (RendezVousImpl rendezVousImpl: rendezvous) {
+            rendezVousClone.add(rendezVousImpl.clone());
+        }
+
+        return rendezVousClone;
     }
 
     @Override
